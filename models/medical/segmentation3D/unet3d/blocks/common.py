6d15b0b80500668bf57aef31272e8ce7f8a9be1c
workhorse <aloys.portafaix@gmail.com>
2022-11-20 00:32:36 +0100

    fix weight standardization 3d

--- models/medical/segmentation3D/unet3d/blocks/common.py
+++ models/medical/segmentation3D/unet3d/blocks/common.py
@@ -15,10 +15,9 @@ class WSConv3d(nn.Conv3d):
 
     def forward(self, x):
         weight = self.weight
-        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2,
-                                                            keepdim=True).mean(dim=3, keepdim=True)
+        weight_mean = weight.mean(dim=(1, 2, 3, 4), keepdim=True)
         weight = weight - weight_mean
-        std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
+        std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1, 1) + 1e-5
         weight = weight / std.expand_as(weight)
         return F.conv3d(x, weight, self.bias, self.stride,
                         self.padding, self.dilation, self.groups)
